4ace0b3187f71edc21756a29ea2af901840cb65a
Ivo Majic <1269362+IvoMajic@users.noreply.github.com>
2024-03-23 13:31:11 +0100

    Improve JSON MediaType parsing

--- src/main/java/com/treblle/spring/TreblleFilter.java
+++ src/main/java/com/treblle/spring/TreblleFilter.java
@@ -27,12 +27,16 @@ public class TreblleFilter extends OncePerRequestFilter {
 
   @Override
   protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
-    String contentType = request.getContentType();
-    if (contentType != null && !MediaType.APPLICATION_JSON_VALUE.equals(contentType)) {
+    if (!isJSONRequest(request)) {
       log.debug("Attempted to intercept request but content type was not valid. Treblle only works on JSON API's.");
       return true;
     }
     return super.shouldNotFilter(request);
+  }
+
+  private boolean isJSONRequest(HttpServletRequest request) {
+    String contentType = request.getContentType();
+    return contentType != null && contentType.startsWith(MediaType.APPLICATION_JSON_VALUE);
   }
 
   @Override
